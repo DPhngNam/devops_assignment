4383e8caa3c573f7d206c6d52ec40bee74333032
Đoàn Phương Nam <120766900+DPhngNam@users.noreply.github.com>
2025-05-04 23:42:34 +0700

    update output

--- modules/security/outputs.tf
+++ modules/security/outputs.tf
@@ -5,3 +5,27 @@ output "public_sg_id" {
 output "private_sg_id" {
   value = aws_security_group.private_ec2_sg.id
 }
+
+output "public_sg_name" {
+  value = aws_security_group.public_ec2_sg.name
+}
+
+output "private_sg_name" {
+  value = aws_security_group.private_ec2_sg.name
+}
+
+output "public_sg_vpc_id" {
+  value = aws_security_group.public_ec2_sg.vpc_id
+}
+
+output "private_sg_vpc_id" {
+  value = aws_security_group.private_ec2_sg.vpc_id
+}
+
+output "public_sg_ingress_count" {
+  value = length(aws_security_group.public_ec2_sg.ingress)
+}
+
+output "private_sg_ingress_count" {
+  value = length(aws_security_group.private_ec2_sg.ingress)
+}
--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -3,6 +3,11 @@ output "vpc_id" {
   value       = aws_vpc.vpc.id
 }
 
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = aws_vpc.vpc.cidr_block
+}
+
 output "private_subnet_ids" {
   description = "List of private subnet IDs"
   value       = [for subnet in aws_subnet.private_subnet : subnet.id]
@@ -16,4 +21,19 @@ output "public_subnet_ids" {
 output "default_security_group_id" {
   description = "The ID of the default security group"
   value       = aws_security_group.default.id
+}
+
+output "default_sg_name" {
+  description = "The name of the default security group"
+  value       = aws_security_group.default.name
+}
+
+output "private_subnet_count" {
+  description = "Number of private subnets"
+  value       = length(aws_subnet.private_subnet)
+}
+
+output "public_subnet_count" {
+  description = "Number of public subnets"
+  value       = length(aws_subnet.public_subnet)
 }
